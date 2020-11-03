e71669c258fee407308ee2af79cfa8bd8da853d9
Nainakumari <iit2019004@iiita.ac.in>
2020-11-03 11:51:00 +0530

    modified timer

--- app/src/main/java/com/example/kloak/Timer.java
+++ app/src/main/java/com/example/kloak/Timer.java
@@ -12,13 +12,29 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class Timer extends AppCompatActivity {
     SeekBar timerSeekBar;
-    TextView timerTextView;
-    Button controllerButton;
-    ImageView imageView4;
-    Boolean counterIsActive = false;
-    CountDownTimer countDownTimer;
+    private static final long start_time=30000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis=start_time;
+
+
+    /*
+        SeekBar timerSeekBar;
+        TextView timerTextView;
+        Button controllerButton;
+        ImageView imageView4;
+        Boolean counterIsActive = false;
+        CountDownTimer countDownTimer;
 
     public void resetTimer() {
 
@@ -57,6 +73,7 @@ public class Timer extends AppCompatActivity {
             counterIsActive = true;
             timerSeekBar.setEnabled(false);
             controllerButton.setText("Stop");
+            counter=true;
 
             countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
 
@@ -69,8 +86,10 @@ public class Timer extends AppCompatActivity {
                 public void onFinish() {
                     timerTextView.setAlpha(0.6f);
                     timerTextView.animate().alpha(1f).setDuration(500);
+                    controllerButton.setText("Reset");
                     MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.b1);
                     mplayer.start();
+
                     resetTimer();
 
                 }
@@ -79,18 +98,15 @@ public class Timer extends AppCompatActivity {
         } else {
             resetTimer();
         }
-    }
+    }*/
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        imageView4=findViewById(R.id.imageView4);
-        timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
-        timerTextView = (TextView)findViewById(R.id.timerTextView);
-        controllerButton = (Button)findViewById(R.id.controllerButton);
-
+        //imageView4=findViewById(R.id.imageView4);
+        /*timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
 
@@ -98,7 +114,7 @@ public class Timer extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
-                updateTimer(progress);
+                updateCountDownText(progress);
 
             }
 
@@ -111,8 +127,71 @@ public class Timer extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
 
             }
+        });*/
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mButtonStartPause=findViewById(R.id.button_start_pause);
+        mButtonReset=findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mTimerRunning){
+                    pauseTimer();
+                }else{
+                    startTimer();
+                }
+            }
         });
 
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+        updateCountDownText();
+    }
+    private void startTimer(){
+        mCountDownTimer=new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long l) {
+                mTimeLeftInMillis=l;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning=false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning=true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
     }
 
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer(){
+        mTimeLeftInMillis=start_time;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText(){
+        int minutes =(int)(mTimeLeftInMillis/1000)/60;
+        int seconds =(int)(mTimeLeftInMillis/1000)%60;
+        String timeLeftFormatted=String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
 }
